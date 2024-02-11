c9af1aed2f6ab039d2088879c4216dac1aa6ffbc
Bruno Morici M <bmorici@usm.cl>
2024-02-11 14:10:05 -0300

    quiero llorar

--- panelBM/settings.py
+++ panelBM/settings.py
@@ -95,10 +95,9 @@ WSGI_APPLICATION = 'panelBM.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
