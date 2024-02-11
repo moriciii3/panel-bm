0fe49ec20aa1cac2cdf4039b3a6ee54be3537f35
Bruno Morici M <bmorici@usm.cl>
2024-02-11 14:06:29 -0300

    no doy mas

--- panelBM/settings.py
+++ panelBM/settings.py
@@ -96,7 +96,8 @@ WSGI_APPLICATION = 'panelBM.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default=os.environ.get('DATABASE_URL')
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
     )
 }
 
